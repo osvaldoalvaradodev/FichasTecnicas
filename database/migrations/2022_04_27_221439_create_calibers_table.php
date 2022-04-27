bd39d71487f365c32895d3d9a4749891aa39236e
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2022-04-27 18:16:39 -0400

    añade name a la migracion de calibers

--- database/migrations/2022_04_27_221439_create_calibers_table.php
+++ database/migrations/2022_04_27_221439_create_calibers_table.php
@@ -16,6 +16,8 @@ class CreateCalibersTable extends Migration
         Schema::create('calibers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('name');
         });
     }
 
