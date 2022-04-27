fb7d40d825d9503854c3735db5bc989c4a4de09f
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2022-04-27 18:15:09 -0400

    Crea modelo Caliber y Datasheet

---
+++ database/factories/CaliberFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Caliber;
+use Faker\Generator as Faker;
+
+$factory->define(Caliber::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/DataSheetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DataSheet;
+use Faker\Generator as Faker;
+
+$factory->define(DataSheet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
